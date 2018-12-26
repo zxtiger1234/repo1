4f05d163b4aca1320c825db2141d7e2226d62790
漠纨 <1934523178@qq.com>
2018-12-26 18:50:03 +0800

    把本地工程再次添加到git

--- src/com/itheima/git/FirstClass.java
+++ src/com/itheima/git/FirstClass.java
@@ -4,4 +4,8 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("HelloWorld.....");
     }
+
+    public void test(){
+        System.out.println("helloworld");
+    }
 }
